cce40e4247f97aa5c31d4dbd32984be7c8e98398
bhoomigundecha <bhoomigundecha@gmail.com>
2026-02-21 13:46:19 +0530

    Avatars on landing

--- landing/src/App.jsx
+++ landing/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Characters from "./components/Characters.jsx";
 import Worlds from "./components/Worlds.jsx";
 import WorldScene from "./components/worlds/WorldScene.jsx";
-// at this point ye bas landing page and re route krra hai 
-// and as such humne kuch add nahi kia hai isme 
+// at this point ye bas landing page and re route krra hai
+// and as such humne kuch add nahi kia hai isme
 function LandingPage() {
   return (
     <div className="min-h-screen w-full overflow-x-hidden">
@@ -12,6 +13,7 @@ function LandingPage() {
       <div className="relative bg-[radial-gradient(ellipse_80%_60%_at_50%_20%,#dbeafe_0%,#c7d7fe_30%,#bfcffe_55%,#e0e7ff_75%,#f0f4ff_100%)]">
         <Navbar />
         <Hero />
+        <Characters />
         <Worlds />
       </div>
     </div>
